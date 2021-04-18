a6c5a795b8234260258d400a090b42812170afd3
Gary Alway <garyalway@gmail.com>
2021-04-18 19:57:39 +0100

    remove build

--- .build/src/clients/getClients.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getDynamoClient = void 0;
-const dynamoClient_1 = require("./dynamoClient");
-const dynamoInstance = Object.freeze(dynamoClient_1.createDynamoClient());
-const getDynamoClient = () => dynamoInstance;
-exports.getDynamoClient = getDynamoClient;
-//# sourceMappingURL=getClients.js.map
--- .build/src/utils/truthy.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.truthy = void 0;
-const truthy = (value) => Boolean(value);
-exports.truthy = truthy;
-//# sourceMappingURL=truthy.js.map
