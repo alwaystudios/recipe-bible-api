8c6bcda9e15da39f0d2e4db487d483c8c4da4ef4
Gary Alway <garyalway@gmail.com>
2021-06-26 13:26:50 +0100

    wip: s3 permissions issue

--- src/domain/contentService.ts
+++ src/domain/contentService.ts
@@ -1,4 +1,5 @@
 import { getS3Client } from '../clients/getClients'
+import { getLogger } from '../clients/logger'
 import { resizeImage } from '../clients/sharpClient'
 
 export type AssetType = 'recipe' | 'ingredient' | 'step'
@@ -21,16 +22,28 @@ export const uploadImage = async ({
   overwrite = true,
 }: UploadImage): Promise<void> => {
   const client = getS3Client()
+  const logger = getLogger()
+  logger.info({
+    filename,
+    folder,
+    type,
+    assetType,
+    overwrite,
+  })
 
   const location = `${folder}/${filename}`
   const exists = await client.objectExists(location)
+  logger.info(`exists? ${JSON.stringify(exists)}`)
 
   if (exists && !overwrite) {
     throw new Error('S3 object already exists')
   }
 
   const width = assetType === 'recipe' ? 1000 : 500
+  logger.info('resize the image')
   const resizedImage = await resizeImage(data, width)
+  logger.info('resized, now save')
 
   await client.putObject(location, resizedImage, type)
+  logger.info('saved')
 }
