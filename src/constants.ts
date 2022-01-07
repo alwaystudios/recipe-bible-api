57da1db722f72148b31aa98193c732ef306846d5
Gary Alway <garydouglasalway@gmail.com>
2022-01-07 21:46:36 +0000

    bumps libs

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const KID = 'LyBJKVRB7d0fsZH5jSbLa'
 export const DDB_TABLE_NAME = 'recipe-bible'
 export const LOCAL_BASE_URL = 'http://localhost:21111'
 export const LOCAL_AWS_CONFIG = {
-  region: 'eu-west-1',
+  region: 'us-east-1',
   accessKeyId: 'root',
   secretAccessKey: 'root',
   endpoint: 'http://localhost:4566',
